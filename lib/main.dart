ef3b8e0debb4114d02b6f387a2cd6636660a76a2
Sagar Shende <sagarshende631@gmail.com>
2020-05-01 23:04:21 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -13,36 +13,36 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
         home: Scaffold(
-          appBar: AppBar(),
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text('Image Slider'),
+          ),
           body: Column(
             children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(
-                    top: 0.0, left: 10.0, right: 10.0, bottom: 35.0),
-                child: Container(
-                  height: 180.0,
-                  child: Carousel(
-                    boxFit: BoxFit.cover,
-                    dotColor: Colors.transparent,
-                    dotSize: 5.5,
-                    dotSpacing: 16.0,
-                    dotBgColor: Colors.transparent,
-                    showIndicator: false,
-                    overlayShadow: false,
-                    overlayShadowColors: Colors.white.withOpacity(0.9),
-                    overlayShadowSize: 0.9,
-                    images: [
-                      AssetImage("assets/img/bannerMan1.png"),
-                      AssetImage("assets/img/bannerMan2.png"),
-                      AssetImage("assets/img/bannerMan3.png"),
-                      AssetImage("assets/img/bannerMan4.png"),
-                    ],
-                  ),
+              Container(
+                height: 180.0,
+                child: Carousel(
+                  boxFit: BoxFit.cover,
+                  dotColor: Colors.transparent,
+                  dotSize: 5.5,
+                  dotSpacing: 16.0,
+                  dotBgColor: Colors.transparent,
+                  showIndicator: false,
+                  overlayShadow: false,
+                  overlayShadowColors: Colors.white.withOpacity(0.9),
+                  overlayShadowSize: 0.9,
+                  images: [
+                    AssetImage("assets/img/bannerMan1.png"),
+                    AssetImage("assets/img/bannerMan2.png"),
+                    AssetImage("assets/img/bannerMan3.png"),
+                    AssetImage("assets/img/bannerMan4.png"),
+                  ],
                 ),
               ),
             ],
